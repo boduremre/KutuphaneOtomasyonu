ca6cc4071248794011e56a9dbfd63a5f217bd626
Emre Bodur <emrebodurbto@gmail.com>
2021-12-05 12:50:02 +0300

    Kitap Tablosuna Yerbilgisi ve Demirbas Kolonlari Eklendi. Migration oluşturuldu.

---
+++ KutuphaneOtomasyonu/Migrations/20211205093257_KitapTablosunaYerbilgisiveDemirbasKolonlariEklendi.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KutuphaneOtomasyonu.Migrations
+{
+    public partial class KitapTablosunaYerbilgisiveDemirbasKolonlariEklendi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DemirbasNo",
+                table: "Kitap",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "YerBilgisi",
+                table: "Kitap",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DemirbasNo",
+                table: "Kitap");
+
+            migrationBuilder.DropColumn(
+                name: "YerBilgisi",
+                table: "Kitap");
+        }
+    }
+}
