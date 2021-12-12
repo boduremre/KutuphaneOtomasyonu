8bbd78ffef9fd39dda55bf4c9a95d2244b3f86c4
Emre Bodur <emrebodurbto@gmail.com>
2021-12-12 19:31:33 +0300

    Add OduncAdet Column To KitapTable

---
+++ KutuphaneOtomasyonu/Migrations/20211212155819_AddOduncAdetColumnToKitapTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KutuphaneOtomasyonu.Migrations
+{
+    public partial class AddOduncAdetColumnToKitapTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OduncAdet",
+                table: "Kitap",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OduncAdet",
+                table: "Kitap");
+        }
+    }
+}
